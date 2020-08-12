76c887a1446b49279ad59473cd6b172044bf5468
gkoberger <gkoberger@gmail.com>
2020-08-12 12:19:49 -0700

    How to add a company

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,4 +1,9 @@
 $.page('index', function() {
+  $('#add-your-company').click(function() {
+    $('#add-your-company').hide();
+    $('#howto').show();
+    return false;
+  });
   //setInterval(function() {
     //$('#parent').toggleClass('on', !$('#parent').hasClass('on'));
   //}, 1400);
