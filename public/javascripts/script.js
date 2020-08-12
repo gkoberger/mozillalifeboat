177f47856f48d2977eda4701b85859f06fe4a9ef
gkoberger <gkoberger@gmail.com>
2020-08-11 21:13:43 -0700

    First commit

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,3 +1,5 @@
 $.page('index', function() {
-
+  setInterval(function() {
+    $('#parent').toggleClass('on', !$('#parent').hasClass('on'));
+  }, 1400);
 });
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: 'Home', svg });
 });
+
+const svg = function (src, _prefix) {
+  if (!src.match(/\.svg$/)) throw new Error("Hey! You can't do that!");
+
+  const prefix = _prefix || 'svg';//rand(7);
+
+  let file = fs.readFileSync(src).toString();
+
+  // Sketch uses generic IDs that conflict...
+  file = file.replace(/id="/g, `id="${prefix}-`);
+  file = file.replace(/xlink:href="#/g, `xlink:href="#${prefix}-`);
+  file = file.replace(/url\(#/g, `url(#${prefix}-`);
+
+  // Give it an ID
+  if (_prefix) {
+    file = file.replace(/<svg/, `<svg id="${_prefix}"`);
+  }
+  return file;
+};
 
 module.exports = router;
